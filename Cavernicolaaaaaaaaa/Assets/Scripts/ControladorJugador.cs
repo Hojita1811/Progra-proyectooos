db8dcfb40011066a2fd8377fed1b807322f93929
Hojita1811 <142283837+Hojita1811@users.noreply.github.com>
2023-10-26 02:40:19 -0600

    Ahora sí está chido

---
+++ Cavernicolaaaaaaaaa/Assets/Scripts/ControladorJugador.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorJugador : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float velocidadCaminar = 3f;
+    public float fuerzaSalto = 50f;
+    public bool enPiso = false;
+    public float saltosMax = 2f;
+
+    private Rigidbody2D miCuerpo;
+    //private SpriteRenderer cavernicola;
+    private Animator miAnimador;
+    private float saltosRest;
+    private ReproductorSonidos misSonido;
+
+    void Start()
+    {
+        miCuerpo = GetComponent<Rigidbody2D>();
+        // cavernicola = GetComponent<SpriteRenderer>();
+        miAnimador = GetComponent<Animator>();
+        misSonido = GetComponent<ReproductorSonidos>();
+        saltosRest = saltosMax;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //La comprobacion de piso
+        //es lo primero que se hace
+        //cada frame
+        comprobarPiso();
+
+        float velActualVert = miCuerpo.velocity.y;
+        //eje horizontal de las flechas
+        float movHoriz = Input.GetAxis("Horizontal");
+
+        if (movHoriz > 0)//a la derecha
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            miCuerpo.velocity = new Vector3(velocidadCaminar, velActualVert, 0);
+            //cavernicola.flipX = false;
+            miAnimador.SetBool("Caminando", true);
+        }
+        else if (movHoriz < 0)//a la izquierda
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            miCuerpo.velocity = new Vector3(-velocidadCaminar, velActualVert, 0);
+            //cavernicola.flipX = true;
+            miAnimador.SetBool("Caminando", true);
+        }
+
+        else//quieto
+        {
+            miCuerpo.velocity = new Vector3(0, velActualVert, 0);
+            miAnimador.SetBool("Caminando", false);
+        }
+
+        if(enPiso)
+        {
+            saltosRest = saltosMax;
+            miAnimador.SetBool("Piso", true);
+        }
+        else if (enPiso==false)
+        {
+            miAnimador.SetBool("Piso", false);
+        }
+
+        if (Input.GetButtonDown("Jump") && saltosRest > 0)
+        {
+            saltosRest--;
+            miCuerpo.AddForce(new Vector3(0, fuerzaSalto, 0), ForceMode2D.Impulse);
+            miAnimador.SetBool("Piso", false);
+            misSonido.reproducir("SALTAR");
+        }
+
+        miAnimador.SetFloat("Vel_Vert", velActualVert);
+        
+    }
+    public void comprobarPiso()
+    {
+        //Lanzo un rayo de deteccion
+        //de colisiones hacia abajo
+        //desde la posicion del este
+        //objeto (cavernicola)
+        enPiso = Physics2D.Raycast(
+            transform.position,//desde donde
+            Vector2.down,//hacia abajo
+            0.1f);//distancia
+    }
+}
---
+++ Cavernicolaaaaaaaaa/Assets/Scripts/Personaje.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+    public int hp = 60;
+    public int hpMax = 100;
+    public int score = 0;
+    public int vidas = 3;
+    public GameObject efectoSangrePrefab;
+    Animator miAnimador;
+    private ReproductorSonidos misSonido;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        miAnimador = GetComponent<Animator>();
+        misSonido = GetComponent<ReproductorSonidos>();
+    }
+
+    // Update is called once per frame
+    public void hacerDanio(int puntos, GameObject atacante)
+    {
+        print(name + "recibe daño de "
+            + puntos + " por " + atacante.name);
+
+        //resto los puntos al HP actual
+        hp = hp - puntos;
+        miAnimador.SetTrigger("DAÑAR");
+
+        //Creo una instancia de la part de sangre
+        GameObject sangre = Instantiate(
+            efectoSangrePrefab, transform);
+
+        misSonido.reproducir("DAÑAR");
+    }
+
+    public void morirAgua(int vidaPerdida, GameObject atacante)
+    {
+        vidas = vidas - vidaPerdida;
+        hp = 0;
+        misSonido.reproducir("MORIR");
+    }
+}
